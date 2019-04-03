a7e5afea5c595f80d16b6a7b64c0c14753c353a0
John Benfer <jbenfer@ksu.edu>
2019-04-03 10:32:40 -0500

    Add project files.

---
+++ NUnit.VendingMachine/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.VendingMachine
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            
+
+            VendingMachine vending = new VendingMachine();
+
+        }
+    }
+}
